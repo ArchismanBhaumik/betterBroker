e713c33f442cdb5a764eeba032de1a60d02dd031
ArchismanBhaumik <archismanbhaumik786@gmail.com>
2023-01-04 12:17:20 +0530

    added white text

--- src/Component/Body.jsx
+++ src/Component/Body.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import { Routes, Route } from "react-router-dom";
 import Posts from './Posts';
 import Favourites from './Favourites';
+import PostAd from './PostAd';
+import ListProperty from './ListProperty';
 
 function Body() {
   return ( <div>
     <Routes>
         <Route path='' element={<Posts/>}/>
         <Route path='favourites' element={<Favourites/>}/>
-      
+        <Route path='post' element={<PostAd/>}/>
+        <Route path='list' element={<ListProperty/>}/>
       </Routes>
   </div> );
 }
--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -1,12 +1,18 @@
+import { Link } from "react-router-dom";
 function Footer() {
     return ( 
     <div className="footer bg-secondary text-center text-white fixed-bottom">
         <div className="row py-4">
             <div className="col-7">
-                <p className="p-0 m-0">Properties listed by you</p>
+            <Link to='/list'>
+                <p className="p-0 m-0 text-white">Properties listed by you</p>
+                </Link>
             </div>
             <div className="col-5">
-                <p className="p-0 m-0">Post an add</p>
+                <Link to='/post'>
+                <p className="p-0 m-0 text-white">Post an add</p>
+                </Link>
+                
             </div>
         </div>
     </div> 
