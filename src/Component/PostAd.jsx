756e3fe851b57e1c9e9fda1786c096999d2eb721
ArchismanBhaumik <archismanbhaumik786@gmail.com>
2023-01-20 17:52:08 +0530

    added postAd

--- src/Component/PostAd.jsx
+++ src/Component/PostAd.jsx
@@ -2,7 +2,33 @@ import React from 'react'
 
 function PostAd() {
   return (
-    <div>PostAd</div>
+    <div className='postad-form'>
+        <h1>BetterBroker.Com</h1>
+        <div>
+          <div>
+          <label className='pName'>Name of Property</label>
+          </div>
+          <input type="text" name='pname' id='pname' placeholder='Enter property name' className='rounded' />
+        </div>
+          
+        <div>
+          <div>
+          <label className='address'>Address</label>
+          </div>
+          <input type="text" name='email' id='email' placeholder='Enter Email' className='rounded'/>
+          
+        </div>
+        <div>
+          <div>
+          <label className='imageOfProperty'>upload photos</label>
+          </div>
+          <input type="file" name='imageProp' id='imageProp'  />
+        </div>
+        
+        <br></br>
+          <button>Post</button>
+          
+    </div>
   )
 }
 
